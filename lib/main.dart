0feb7b1154402dc694f6227ed522ff7f513f2e73
Sudharsshan S Y <sudharsshansy@gmail.com>
2024-02-29 22:44:52 +0530

    added state and progress bar

--- lib/main.dart
+++ lib/main.dart
@@ -237,3 +237,81 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class ProgressBarWidget extends StatefulWidget{
+  @override
+  _ProgressBarWidget createState() => _ProgressBarWidget();
+}
+
+class _ProgressBarWidget extends State<ProgressBarWidget> with SingleTickerProviderStateMixin{
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3), // Duration for one cycle of animation (from empty to full)
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.reset(); // Reset the animation when it's completed to create a loop
+          _controller.forward();
+        }
+      });
+    _controller.forward(); // Start the animation
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the animation controller
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity, // Full width
+          height: 80, // Increased height for a thicker progress bar
+          decoration: BoxDecoration(
+            color: Colors.grey[300], // Background color of the progress bar container
+            borderRadius: BorderRadius.circular(20), // Rounded corners
+          ),
+          child: FractionallySizedBox(
+            alignment: Alignment.centerLeft,
+            widthFactor: _animation.value, // Use the animated value for the width factor
+            child: Container(
+              height: 80, // Increased height for a thicker progress bar
+              decoration: BoxDecoration(
+                color: Colors.blue, // Color of the progress bar
+                borderRadius: BorderRadius.circular(20), // Rounded corners
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Center(
+            child: Text(
+              'Downloading...', // Example text
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/test_demo_files/ProgressBarWidget.dart
+++ lib/test_demo_files/ProgressBarWidget.dart
@@ -1,3 +1,4 @@
+import 'package:batterymanagementsystem/theme.dart';
 import 'package:flutter/material.dart';
 
 class ProgressBarWidget extends StatefulWidget {
@@ -6,80 +7,160 @@ class ProgressBarWidget extends StatefulWidget {
 }
 
 class _ProgressBarWidgetState extends State<ProgressBarWidget> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+  late AnimationController _controller1, _controller2, _controller3;
+  late Animation<double> _animation1, _animation2, _animation3;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
+
+    //Widget 1
+    _controller1 = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 3), // Duration for one cycle of animation (from empty to full)
+      duration: const Duration(milliseconds: 800), // Duration for one cycle of animation (from empty to full)
     );
-    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller)
+    _animation1 = Tween<double>(begin: 0.0, end: 1.0).animate(_controller1)
       ..addListener(() {
         setState(() {});
       })
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          _controller.reset(); // Reset the animation when it's completed to create a loop
-          _controller.forward();
+          _controller1.reset(); // Reset the animation when it's completed to create a loop
+          _controller1.forward();
         }
       });
-    _controller.forward(); // Start the animation
+    _controller1.forward(); // Start the animation
+
+    //Widget 2
+    _controller2 = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200), // Duration for one cycle of animation (from empty to full)
+    );
+    _animation2 = Tween<double>(begin: 0.0, end: 1.0).animate(_controller2)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller2.reset(); // Reset the animation when it's completed to create a loop
+          _controller2.forward();
+        }
+      });
+    _controller2.forward(); // Start the animation
+
+    //Widget 3
+    _controller3 = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1600), // Duration for one cycle of animation (from empty to full)
+    );
+    _animation3 = Tween<double>(begin: 0.0, end: 1.0).animate(_controller3)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller3.reset(); // Reset the animation when it's completed to create a loop
+          _controller3.forward();
+        }
+      });
+    _controller3.forward(); // Start the animation
   }
 
   @override
   void dispose() {
-    _controller.dispose(); // Dispose the animation controller
+    _controller1.dispose(); // Dispose the animation controller
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          width: double.infinity, // Full width
-          height: 40, // Increased height for a thicker progress bar
-          decoration: BoxDecoration(
-            color: Colors.grey[300], // Background color of the progress bar container
-            borderRadius: BorderRadius.circular(20), // Rounded corners
-          ),
-          child: FractionallySizedBox(
-            alignment: Alignment.centerLeft,
-            widthFactor: _animation.value, // Use the animated value for the width factor
-            child: Container(
-              height: 40, // Increased height for a thicker progress bar
-              decoration: BoxDecoration(
-                color: Colors.blue, // Color of the progress bar
-                borderRadius: BorderRadius.circular(20), // Rounded corners
+    return Center(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: double.infinity, // Full width
+                height: 80, // Increased height for a thicker progress bar
+                margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 10), //change this value as per your choice for making neat looking widget
+                decoration: BoxDecoration(
+                  color: Colors.grey[300], // Background color of the progress bar container
+                  borderRadius: BorderRadius.circular(20), // Rounded corners
+                ),
+                child: FractionallySizedBox(
+                  alignment: Alignment.centerLeft,
+                  widthFactor: _animation1.value, // Use the animated value for the width factor
+                  child: Container(
+                    height: 80, // Increased height for a thicker progress bar
+                    decoration: BoxDecoration(
+                      color: Colors.blue, // Color of the progress bar
+                      borderRadius: BorderRadius.circular(20), // Rounded corners
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          child: Center(
-            child: Text(
-              'Downloading...', // Example text
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+              const Positioned(
+                top: 25,
+                left: 50,
+                right: 0,
+                bottom: 0,
+                child: Text(
+                  'Downloading...', // Example text
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
-        ),
-      ],
+          Stack(
+            children: [
+              Container(
+                width: double.infinity, // Full width
+                height: 80, // Increased height for a thicker progress bar
+                margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 10), //change this value as per your choice for making neat looking widget
+                decoration: BoxDecoration(
+                  color: Colors.grey[300], // Background color of the progress bar container
+                  borderRadius: BorderRadius.circular(20), // Rounded corners
+                ),
+                child: FractionallySizedBox(
+                  alignment: Alignment.centerLeft,
+                  widthFactor: _animation1.value, // Use the animated value for the width factor
+                  child: Container(
+                    height: 80, // Increased height for a thicker progress bar
+                    decoration: BoxDecoration(
+                      color: Colors.blue, // Color of the progress bar
+                      borderRadius: BorderRadius.circular(20), // Rounded corners
+                    ),
+                  ),
+                ),
+              ),
+              const Positioned(
+                top: 25,
+                left: 50,
+                right: 0,
+                bottom: 0,
+                child: Text(
+                  'Downloading...', // Example text
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeClass.lightTheme,
+    darkTheme: ThemeClass.darkTheme,
     home: Scaffold(
       appBar: AppBar(
         title: Text('Progress Bar Demo'),
