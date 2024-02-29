6bd66e4d8cf58c629da5bccfbba35e262072007a
ADITI <aditirkrishna@gmail.com>
2024-02-29 17:01:25 +0530

    added an import statement

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:workmanager/workmanager.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter/material.dart';
 
 //This var stores data from the api response received
 String responseData = 'Loading...';
